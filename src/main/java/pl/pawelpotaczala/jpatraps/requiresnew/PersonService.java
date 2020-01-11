d74e0045d505d15b23a9e259fd794bfe6f21488a
Paweł Potaczała <paul.potaczala@gmail.com>
2020-01-12 00:29:48 +0100

    add spock tests and update java version to 11

--- src/main/java/pl/pawelpotaczala/jpatraps/requiresnew/PersonService.java
+++ src/main/java/pl/pawelpotaczala/jpatraps/requiresnew/PersonService.java
@@ -1,21 +1,18 @@
-package pl.michalmarciniec.jpatraps.requiresnew;
+package pl.pawelpotaczala.jpatraps.requiresnew;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class PersonService {
 
     private final PersonRepository personRepository;
     private final WalletService walletService;
 
-    @Autowired
-    public PersonService(PersonRepository personRepository, WalletService walletService) {
-        this.personRepository = personRepository;
-        this.walletService = walletService;
-    }
-
+    // Spring checks if there is an active transaction, then it creates a new one if nothing existed.
+    // Otherwise, the business logic appends to the currently active transaction
     @Transactional
     public long createPerson(String name) {
         Person person = new Person(name);
